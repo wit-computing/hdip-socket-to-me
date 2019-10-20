3c03044e373288a25ca6c7de27b33cda9c80239e
“fxwalsh“ <fxwalsh@wit.ie>
2019-10-20 23:45:11 +0100

    updates python

--- python/client-simple.py
+++ python/client-simple.py
@@ -1,23 +1,19 @@
 import socket
 import sys
 
-HOST, PORT = "ec2-34-253-239-101.eu-west-1.compute.amazonaws.com", 50100
+HOST, PORT = "192.168.5.2", 50200
 data = " ".join(sys.argv[1:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
-
-    # Receive data from the server and shut down
-    received = sock.recv(1024)
-finally:
-    sock.close()
-
+# Connect to server and send data
+sock.connect((HOST, PORT))
+sock.sendall(data + "\n")
 print "Sent:     {}".format(data)
-print "Received: {}".format(received)
-
-
+while True:
+        # Receive data from the server and shut down
+        received = sock.recv(1024)
+        if received:
+            print "Received: {}".format(received)
+	else:
+	    break
--- python/server-simple.py
+++ python/server-simple.py
@@ -1,4 +1,6 @@
 import SocketServer
+import random
+import time
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
@@ -12,21 +14,31 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
+        file = open("lots-of-data.txt","r")
         print "{} wrote:".format(self.client_address[0])
-        if  self.data == "1":
-            file = open("lots-of-data.txt","r")  
-            data = file.read()
-        else:
-            data = "42"     
         print self.data
-        self.request.sendall(data)
+	# i=random.randint(100,200)
+        i=200
+        if  self.data:
+            timeToSleep = float(self.data)
+            for j in range(1,i):
+               line = file.readline()
+               self.request.sendall(line)
+               print "{} sent:".format(line)
+               time.sleep(timeToSleep)
+        else:
+            self.request.sendall(file.read())
+            print "{} wrote:".format(self.client_address[0])
+
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "", 50200
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-
+    print("Server up and running...")
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
